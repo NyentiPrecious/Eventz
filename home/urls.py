87c8b29900e5f10c3e9239bbfd98ba80a10107a8
NyentiPrecious <gildanyenti@gmail.com>
2023-01-10 22:00:51 +0100

    eventz

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('event_list', views.event_list, name='event_list'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 def home(request):
         return render(request, 'home.html', {})
+def event_list(request):
+        return render(request, 'event_list.html', {})
 # Create your views here.
